c00186ab53ee71552638a58fee158a29095f00e1
Dandidit <dnyal1421@gmail.com>
2026-08-01 15:48:36 +0800

    change back rate limit to 1 hour

--- api/consultation.js
+++ api/consultation.js
@@ -127,7 +127,7 @@ export async function POST(request) {
         SELECT COUNT(*)::int AS count
         FROM consultation_submissions
         WHERE ip_address = ${ipAddress}::inet
-          AND submitted_at >= NOW() - INTERVAL '1 minute'
+          AND submitted_at >= NOW() - INTERVAL '1 hour'
       `;
 
       if (rateLimitResult[0].count >= 3) {
