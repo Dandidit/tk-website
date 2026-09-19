e8ecc8b129aa65e2c0321cc563b7a1a9fbe57598
Dandidit <dnyal1421@gmail.com>
2026-09-19 22:01:33 +0800

    add analytics for vercel

--- js/pages.js
+++ js/pages.js
@@ -93,3 +93,13 @@ fetch("component/footer.html")
   .catch(error => {
     console.error(error);
   });
+
+// Javascript for vercel analytics
+window.va = window.va || function () {
+  (window.vaq = window.vaq || []).push(arguments);
+};
+
+const script = document.createElement("script");
+script.defer = true;
+script.src = "/_vercel/insights/script.js";
+document.head.appendChild(script);
